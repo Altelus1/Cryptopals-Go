3224410fde6fb47283e62e6d6f147fe8a277b14b
Altelus1 <altelusenyodelus@gmail.com>
2020-07-31 18:24:48 +0800

    Implemented Base64 From Hex without equals sign yet

--- cryptopals/challenges/challenge1.go
+++ cryptopals/challenges/challenge1.go
@@ -8,7 +8,8 @@ import (
 
 func Challenge1(){
 
-  var x = cryptofunctions.Hex_to_bytes("41414141414141ff")
-  fmt.Println(x)
+  var b64 = cryptofunctions.Hex_to_b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d4141")
+  fmt.Println("Result: ", b64)
 
+  fmt.Println("LOL: ", []byte{'x', 'y'}[0])
 }
--- cryptopals/cryptofunctions/crypto_ops.go
+++ cryptopals/cryptofunctions/crypto_ops.go
@@ -1,13 +1,64 @@
 package cryptofunctions
 
-import "math"
+import (
+  "math"
+  "encoding/binary"
+  "fmt"
+)
 
 func Hex_to_b64(hex string) string {
 
   var hex_bytes = Hex_to_bytes(hex)
 
-  
+  return bytes_to_b64(hex_bytes)
 
+}
+
+func bytes_to_b64(byte_arr []byte) string {
+
+//  var add_bytes = []byte{0, 0}
+//  var orig_byte_arr_len = len(byte_arr)
+//  var tribyte_div_length = orig_byte_arr_len - (orig_byte_arr_len % 3)
+
+//  byte_arr = append(byte_arr, add_bytes[0:tribyte_div_length+3-orig_byte_arr_len]...)
+
+  var res_b64 []byte
+  var count = 0
+
+  for count < len(byte_arr) {
+
+    var tmp = make([]byte, 3)
+    copy(tmp, byte_arr[count:count+3])
+
+    var tribyte = binary.BigEndian.Uint32(append([]byte{0}, tmp...))
+
+    var quadcount = 3
+
+    for quadcount >= 0 {
+      var sixbit = byte((tribyte >> (quadcount * 6)) & 0x3f)
+
+      var result_char byte
+
+      if sixbit >= 0 && sixbit <= 25 {
+        result_char = byte(sixbit+byte('A'))
+      } else if sixbit >= 26 && sixbit <= 51 {
+        result_char = byte(sixbit+byte('a')-26)
+      } else if sixbit >= 52 && sixbit <= 61 {
+        result_char = byte(sixbit+byte('0')-52)
+      } else if sixbit == 62 {
+        result_char = '+'
+      } else {
+        result_char = '/'
+      }
+
+      res_b64 = append(res_b64, result_char)
+      quadcount -= 1
+
+    }
+    count += 3
+  }
+
+  return string(res_b64)
 }
 
 func Hex_to_bytes(hex string) []byte {
