ff3bb2b771f5368dbd4fa0f7c9548d54d4b44d09
Altelus1 <altelusenyodelus@gmail.com>
2020-08-01 22:55:23 +0800

    replace naming conventions of variables and names

--- cryptopals/challenges/challenge1.go
+++ cryptopals/challenges/challenge1.go
@@ -8,11 +8,11 @@ import (
 
 func Challenge1Set1(){
 
-  var b64 = cryptofunctions.Hex_to_b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+  var b64 = cryptofunctions.HexToB64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   fmt.Println("Result: ", b64)
 
 }
 
 func Challeng1Set2(){
-  
+
 }
--- cryptopals/cryptofunctions/crypto_ops.go
+++ cryptopals/cryptofunctions/crypto_ops.go
@@ -5,35 +5,35 @@ import (
   "encoding/binary"
 )
 
-func Hex_to_b64(hex string) string {
+func HexToB64(hex string) string {
 
-  var hex_bytes = Hex_to_bytes(hex)
+  var hexBytes = HexToBytes(hex)
 
-  return bytes_to_b64(hex_bytes)
+  return BytesToB64(hexBytes)
 
 }
 
-func bytes_to_b64(byte_arr []byte) string {
+func BytesToB64(byteArr []byte) string {
 
 //  var add_bytes = []byte{0, 0}
-  var orig_byte_arr_len = len(byte_arr)
-  var tribyte_div_length = orig_byte_arr_len - (orig_byte_arr_len % 3)
+  var origByteArrLen = len(byteArr)
+  var triByteDivLength = origByteArrLen - (origByteArrLen % 3)
 
-//  byte_arr = append(byte_arr, add_bytes[0:tribyte_div_length+3-orig_byte_arr_len]...)
+//  byteArr = append(byteArr, add_bytes[0:triByteDivLength+3-origByteArrLen]...)
 
-  var res_b64 []byte
+  var resB64 []byte
   var count = 0
 
-  for count < len(byte_arr) {
+  for count < len(byteArr) {
 
     var tmp = make([]byte, 3)
 
-    copy(tmp, byte_arr[count:count+3])
+    copy(tmp, byteArr[count:count+3])
 
     var tribyte = binary.BigEndian.Uint32(append([]byte{0}, tmp...))
 
-    if count+3 > tribyte_div_length {
-      tmp = tmp[:orig_byte_arr_len-tribyte_div_length]
+    if count+3 > triByteDivLength {
+      tmp = tmp[:origByteArrLen-triByteDivLength]
     }
 
     var quadcount = 3
@@ -41,21 +41,21 @@ func bytes_to_b64(byte_arr []byte) string {
     for quadcount >= 3-len(tmp) {
       var sixbit = byte((tribyte >> (quadcount * 6)) & 0x3f)
 
-      var result_char byte
+      var resultChar byte
 
       if sixbit >= 0 && sixbit <= 25 {
-        result_char = byte(sixbit+byte('A'))
+        resultChar = byte(sixbit+byte('A'))
       } else if sixbit >= 26 && sixbit <= 51 {
-        result_char = byte(sixbit+byte('a')-26)
+        resultChar = byte(sixbit+byte('a')-26)
       } else if sixbit >= 52 && sixbit <= 61 {
-        result_char = byte(sixbit+byte('0')-52)
+        resultChar = byte(sixbit+byte('0')-52)
       } else if sixbit == 62 {
-        result_char = '+'
+        resultChar = '+'
       } else {
-        result_char = '/'
+        resultChar = '/'
       }
 
-      res_b64 = append(res_b64, result_char)
+      resB64 = append(resB64, resultChar)
       quadcount -= 1
 
     }
@@ -65,37 +65,41 @@ func bytes_to_b64(byte_arr []byte) string {
   /*
     Appending equals signs  to base 64
   */
-  res_b64 = append(res_b64, []byte{'=','='}[:(tribyte_div_length+3-orig_byte_arr_len) % 3]...)
+  resB64 = append(resB64, []byte{'=','='}[:(triByteDivLength+3-origByteArrLen) % 3]...)
 
-  return string(res_b64)
+  return string(resB64)
 }
 
-func Hex_to_bytes(hex string) []byte {
+func HexToBytes(hex string) []byte {
 
-  var byte_length = len(hex)/2
-  var return_bytes []byte
+  var byteLength = len(hex)/2
+  var returnBytes []byte
 
-  for count := 0; count < byte_length; count++ {
-    var hex_byte = Hex_to_byte(hex[count*2:(count+1)*2])
-    return_bytes = append(return_bytes, hex_byte)
+  for count := 0; count < byteLength; count++ {
+    var hex_byte = HexToByte(hex[count*2:(count+1)*2])
+    returnBytes = append(returnBytes, hex_byte)
   }
 
-  return return_bytes
+  return returnBytes
 
 }
 
-func Hex_to_byte(hex string) byte {
+func HexToByte(hex string) byte {
 
-  var ret_int = 0
+  var retInt = 0
 
   for count := len(hex)-1; count >= 0; count-- {
     // If it is a-f
     if byte(hex[count]) >= byte('a') {
-      ret_int += int(math.Pow(16, float64(len(hex)-1-count))) * int(byte(hex[count]) - byte('a') + 10)
+      retInt += int(math.Pow(16, float64(len(hex)-1-count))) * int(byte(hex[count]) - byte('a') + 10)
     } else {
-      ret_int += int(math.Pow(16, float64(len(hex)-1-count))) * int(byte(hex[count]) - byte('0'))
+      retInt += int(math.Pow(16, float64(len(hex)-1-count))) * int(byte(hex[count]) - byte('0'))
     }
   }
 
-  return byte(ret_int)
+  return byte(retInt)
+}
+
+func Byte_to_hex(){
+  
 }
